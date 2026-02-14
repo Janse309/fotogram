3b8348481878fedb001d0c22d00538073c4a8f02
janse <jansimoboecker40@gmail.com>
2026-02-14 16:22:22 +0100

    git add array

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+let images = [
+    "ducks.jpg",
+    "grizzli.jpg",
+    "habichtskauz.jpg",
+    "hase.jpg",
+    "hawk.jpg",
+    "horses.jpg",
+    "lamm.jpg",
+    "merkats.jpg",
+    "mole.jpg",
+    "puppys.jpg",
+    "rhino.jpg",
+    "wolves.jpg"
+]
