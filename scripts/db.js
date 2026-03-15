d67430e264b924b86966cf238ad2669e30cc6555
janse <jansimonboecker40@gmail.com>
2026-03-15 14:57:41 +0100

    add json in db.js

---
+++ scripts/db.js
@@ -0,0 +1,50 @@
+let images = [
+    {
+        "name": "Two wolves",
+        "img": "<img src='./img/wolves.jpg' alt='wolfs'>"
+    },
+    {
+        "name": "Baby sheep",
+        "img": "<img src='./img/lamm.jpg' alt='Baby sheep'>"
+    },
+    {
+        "name": "Baby duck",
+        "img": "<img src='./img/ducks.jpg' alt='Baby ducks'>"
+    },
+    {
+        "name": "Owl",
+        "img": "<img src='./img/habichtskauz.jpg' alt='habichtskauz'>"
+    },
+    {
+        "name": "Two dogs",
+        "img": "<img src='./img/puppys.jpg' alt='dogs'>"
+    },
+    {
+        "name": "Rabbit",
+        "img": "<img src='./img/hase.jpg' alt='rabbit'>"
+    },
+    {
+        "name": "Merkats",
+        "img": "<img src='./img/merkats.jpg' alt='meerkats'>"
+    },
+    {
+        "name": "Mole",
+        "img": "<img src='./img/mole.jpg' alt='mole'>"
+    },
+    {
+        "name": "Rhino",
+        "img": "<img src='./img/rhino.jpg' alt='rhino'>"
+    },
+    {
+        "name": "Hawk",
+        "img": "<img src='./img/hawk.jpg' alt='hawk'>"
+    },
+    {
+        "name": "Two bears",
+        "img": "<img src='./img/grizzli.jpg alt='grizzlies'>"
+    },
+    {
+        "name": "Two horses",
+        "img": "<img src='./img/horses.jpg' alt='horses'>"
+    }
+]
